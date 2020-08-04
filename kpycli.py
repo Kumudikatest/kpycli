6adef6307d0e1b82638819fceeeacabbc6d08202
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-08-04 12:13:14 +0530

    add account api

--- kpycli.py
+++ kpycli.py
@@ -1,3 +1,15 @@
+import requests
+
 def handler(event, context):
+    try:
+        res = requests.get(
+            "https://paper-api.alpaca.markets/v2/account",
+            params={},
+            headers={"Accept":"application/json"}
+        )
+        # your code goes here
+    except BaseException as e:
+        # error handling goes here
+        raise(e)
     
     return {"message": "Successfully executed"}
