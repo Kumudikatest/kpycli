6311dd034db916a8dcc5124e245ea54113984580
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-08-04 13:04:00 +0530

    add cog

--- kpycli.py
+++ kpycli.py
@@ -1,3 +1,5 @@
+import boto3
+cognito_idp = boto3.client("cognito-idp")
 import requests
 
 def handler(event, context):
@@ -11,5 +13,13 @@ def handler(event, context):
     except BaseException as e:
         # error handling goes here
         raise(e)
+        try:
+            data = cognito_idp.list_users(
+                UserPoolId="us-east-1_HdYJb7Znp",
+                Limit=10
+            )
+        except BaseException as e:
+            print(e)
+            raise(e)
     
     return {"message": "Successfully executed"}
